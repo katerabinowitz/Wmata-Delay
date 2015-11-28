1acc5e33b90be82c8d29ef03bc13b7677f070e73
katerabinowitz <cathryn.rabinowitz@gmail.com>
2015-11-28 12:30:12 -0500

    delay calcs

--- WMATA BestWorst.R
+++ WMATA BestWorst.R
@@ -1,122 +1,165 @@
 library(stringr)
 library(plyr)
-Delays<-read.csv("/Users/katerabinowitz/Documents/DataLensDC/WMATA-Delay/Wmata-Delay/WMATAService.csv", 
+DelayRaw<-read.csv("/Users/katerabinowitz/Documents/DataLensDC/WMATA-Delay/Wmata-Delay/WMATAService.csv", 
                  stringsAsFactors=FALSE, strip.white=TRUE)
 
-Delays<-subset(Delays,grepl('[a-z]',Delays$Incident) & !(grepl('Report Archives',Delays$Incident)))
+DelayRaw<-subset(DelayRaw,grepl('[a-z]',DelayRaw$Incident) & !(grepl('Report Archives',DelayRaw$Incident)))
 
 ### Dates and Times ###
-Delays$LongDay<-gsub("Daily Service Report for ", "",Delays$Date)
-Date<-as.data.frame(strsplit(Delays$LongDay,", "))
+DelayRaw$LongDay<-gsub("Daily Service Report for ", "",DelayRaw$Date)
+Date<-as.data.frame(strsplit(DelayRaw$LongDay,", "))
 Date<-as.data.frame(t(Date))
 colnames(Date)<-c("Day","MD","Yr")
-DelayV2<-cbind(Delays,Date)
+Delays<-cbind(DelayRaw,Date)
 
-DelayV2<-subset(DelayV2,DelayV2$Incident!="Wednesday November 18, 2014    ")
-DelayV2$DayType<-ifelse(DelayV2$Day %in% c("Saturday","Sunday"), "Weekend","Weekday")
-DelayV2$Month<-gsub( " .*$", "", DelayV2$MD)
+Delays<-subset(Delays,Delays$Incident!="Wednesday November 18, 2014    ")
+Delays$DayType<-ifelse(Delays$Day %in% c("Saturday","Sunday"), "Weekend","Weekday")
+Delays$Month<-gsub( " .*$", "", Delays$MD)
 
-DelayV2$Time<-substr(DelayV2$Incident,1,10)
-DelayV2$Hour<-ifelse(DelayV2$Time=="\n5 57 p.m.",5,
-                     ifelse(DelayV2$Time=="\n* 5:00 p.",5,
-                            as.numeric(gsub(":.*$","",DelayV2$Time))))
+Delays$Time<-substr(Delays$Incident,1,10)
 
-DelayV2$Hour24<-ifelse((grepl("p",DelayV2$Time) & DelayV2$Hour==12),DelayV2$Hour,
-                        ifelse(grepl("p",DelayV2$Time),DelayV2$Hour+12,DelayV2$Hour))
+Delays$Hour<-ifelse(Delays$Time=="\n5 57 p.m.",5,
+                     ifelse(Delays$Time=="\n* 5:00 p.",5,
+                      ifelse(Delays$Time=="\n803 a.m. ",8,
+                            as.numeric(gsub(":.*$","",Delays$Time)))))
 
-DelayV2$TimeGroup<-ifelse(DelayV2$DayType=="Weekday" & DelayV2$Hour24 %in% c(6,7,8,9),"Rush",
-                          ifelse(DelayV2$DayType=="Weekday" & DelayV2$Hour24 %in% c(16,17,18,19),"Rush",
+Delays<-subset(Delays,!is.na(Delays$Hour) & !(Delays$Month=="December"))
+
+Delays$Hour24<-ifelse((grepl("p",Delays$Time) & Delays$Hour==12),Delays$Hour,
+                        ifelse(grepl("p",Delays$Time),Delays$Hour+12,Delays$Hour))
+
+Delays$TimeGroup<-ifelse(Delays$DayType=="Weekday" & Delays$Hour24 %in% c(6,7,8,9),"Rush",
+                          ifelse(Delays$DayType=="Weekday" & Delays$Hour24 %in% c(16,17,18,19),"Rush",
                                   "Off Rush"))
-DelayV2<-DelayV2[c(3,5:9,11:12)]
+Delays<-Delays[c(3,5:9,11:12)]
 
 ### Incident Type ###
-DelayV2$Suspend<-ifelse(grepl("suspend",DelayV2$Incident),1,
-                        ifelse(grepl("Suspend",DelayV2$Incident),1,0))
-DelayV2$Closed<-ifelse(grepl("close",DelayV2$Incident),1,
-                       ifelse(grepl("Close",DelayV2$Incident),1,0))
-DelayV2$Offload<-ifelse(grepl("offload",DelayV2$Incident),1,
-                        ifelse(grepl("Offload",DelayV2$Incident),1,0))
-DelayV2$Shuttle<-ifelse(grepl("Shuttle",DelayV2$Incident),1,
-                        ifelse(grepl("shuttle",DelayV2$Incident),1,0))
-
+#Outcome
+Delays$Suspend<-ifelse(grepl("suspend",Delays$Incident),1,
+                        ifelse(grepl("Suspend",Delays$Incident),1,0))
+Delays$Closed<-ifelse(grepl("close",Delays$Incident),1,
+                       ifelse(grepl("Close",Delays$Incident),1,0))
+Delays$Offload<-ifelse(grepl("offload",Delays$Incident),1,
+                        ifelse(grepl("Offload",Delays$Incident),1,0))
+Delays$Shuttle<-ifelse(grepl("Shuttle",Delays$Incident),1,
+                        ifelse(grepl("shuttle",Delays$Incident),1,0))
+Delays$SingleTrack<-ifelse(grepl("ingle",Delays$Incident),1,0)
+Delays$Express<-ifelse(grepl("xpress",Delays$Incident),1,0)
+Delays$Reroute<-ifelse(grepl("eroute",Delays$Incident),1,
+                       ifelse(grepl("changed to", Delays$Incident),1,0))
+#Issue
+Delays$Issue<-ifelse(grepl("perational problem",Delays$Incident),"Operational",
+                ifelse((grepl("maintenance",Delays$Incident) | grepl("unscheduled track work",Delays$Incident) | grepl("late",Delays$Incident) |
+                grepl("disabled work unit",Delays$Incident)),"Maintenance",
+                  ifelse((grepl("signal problem",Delays$Incident) | grepl("door problem",Delays$Incident) | grepl("equipment problem",Delays$Incident) | 
+                  grepl("brake problem",Delays$Incident) | grepl("break problem",Delays$Incident)| grepl("track problem",Delays$Incident) | 
+                  grepl("power problem",Delays$Incident) | grepl("switch problem",Delays$Incident) | grepl("mechanical problem",Delays$Incident) |
+                  grepl("escalator",Delays$Incident) | grepl("elevator",Delays$Incident) | grepl("cracked rail",Delays$Incident) | 
+                  grepl("cracked third",Delays$Incident)),"Mechnical",
+                      ifelse((grepl("disabled No",Delays$Incident) | grepl("disabled no",Delays$Incident) | grepl("derail",Delays$Incident)),"Trains",
+                          ifelse((grepl("fire",Delays$Incident) | grepl("smoke",Delays$Incident) | grepl("police",Delays$Incident) | 
+                          grepl("emergency",Delays$Incident) | grepl("MTPD",Delays$Incident) | grepl("burning",Delays$Incident) | 
+                          grepl("smolder",Delays$Incident) | grepl("unattended package",Delays$Incident) | 
+                          grepl("suspicious package",Delays$Incident)),"FireCrime",
+                            ifelse((grepl("medical",Delays$Incident) | grepl("sick",Delays$Incident) | grepl("unauthorized person",Delays$Incident) | 
+                            grepl("passenger interfering",Delays$Incident) | grepl("deer",Delays$Incident) | grepl("weather",Delays$Incident)| 
+                            grepl("Presidential",Delays$Incident)| grepl("slippery",Delays$Incident) | grepl("person struck",Delays$Incident) |
+                            grepl("passenger struck",Delays$Incident) | grepl("obstruction",Delays$Incident) | grepl("customer",Delays$Incident) |
+                            grepl("assist a passenger",Delays$Incident) | grepl("assisting a passenger",Delays$Incident) | 
+                            grepl("passenger falling",Delays$Incident) | grepl("child",Delays$Incident) | grepl("bird",Delays$Incident) | 
+                            grepl("dog",Delays$Incident)),"External",
+                                ifelse(grepl("did not operate",Delays$Incident) | grepl("schedule",Delays$Incident),"Schedule",
+                                  "NA")))))))
 ###Delay Time###
-DelayV2$ShortInc<-substr(DelayV2$Incident,11,1000)
-DelayV2$ShortInc<-gsub('minute.*','',DelayV2$ShortInc)
-DelayV2$ShortInc<-gsub(".*\\.m.","",DelayV2$ShortInc)
+Delays$ShortInc<-substr(Delays$Incident,11,1000)
+Delays$ShortInc<-gsub('minute.*','',Delays$ShortInc)
+Delays$ShortInc<-gsub(".*\\.m.","",Delays$ShortInc)
 
-DelayV2$Delay<-as.numeric(str_extract(DelayV2$ShortInc,"[[:digit:]]+"))
+Delays$Delay<-as.numeric(str_extract(Delays$ShortInc,"[[:digit:]]+"))
 
 #where no delay time reported, estimate based on type of incident
-offload<-subset(DelayV2,DelayV2$Offload==1 & !is.na(DelayV2$Delay))
+offload<-subset(Delays,Delays$Offload==1 & !is.na(Delays$Delay))
 median(offload$Delay)
-suspend<-subset(DelayV2,DelayV2$Suspend==1 & !is.na(DelayV2$Delay))
+suspend<-subset(Delays,Delays$Suspend==1 & !is.na(Delays$Delay))
 median(suspend$Delay)
-shuttle<-subset(DelayV2,DelayV2$Shuttle==1 & !is.na(DelayV2$Delay))
+shuttle<-subset(Delays,Delays$Shuttle==1 & !is.na(Delays$Delay))
 median(shuttle$Delay)
+ST<-subset(Delays,Delays$SingleTrack==1 & !is.na(Delays$Delay))
+median(ST$Delay)
 
-DelayV2$Delay<-ifelse((is.na(DelayV2$Delay)& DelayV2$Offload==1),7,
-                  ifelse((is.na(DelayV2$Delay)& DelayV2$Suspend==1),40,
-                    ifelse((is.na(DelayV2$Delay)& DelayV2$Shuttle==1),40,
-                      ifelse((is.na(DelayV2$Delay)& grepl("ignificant delay",DelayV2$Incident)),40,
-                        DelayV2$Delay))))
+Delays$Delay<-ifelse((is.na(Delays$Delay)& Delays$Offload==1),8,
+                  ifelse((is.na(Delays$Delay)& Delays$Suspend==1),40,
+                    ifelse((is.na(Delays$Delay)& Delays$Shuttle==1),40,
+                      ifelse((is.na(Delays$Delay)& Delays$SingleTrack==1),20,
+                          ifelse((is.na(Delays$Delay)& grepl("ignificant delay",Delays$Incident)),40,
+                            ifelse((is.na(Delays$Delay)& grepl("delay",Delays$Incident)),8,
+                        Delays$Delay))))))
 
-###Lines, Routes, and Stations###
-DelayV2$Silver<-ifelse(grepl("Silver",DelayV2$Incident),1,0)
-DelayV2$Orange<-ifelse(grepl("Orange",DelayV2$Incident),1,0)
-DelayV2$Blue<-ifelse(grepl("Blue",DelayV2$Incident),1,0)
-DelayV2$Red<-ifelse(grepl("Red",DelayV2$Incident),1,0)
-DelayV2$Yellow<-ifelse(grepl("Yellow",DelayV2$Incident),1,0)
-DelayV2$Green<-ifelse(grepl("Green",DelayV2$Incident),1,0)
+###Lines and Routes###
+Delays$Silver<-ifelse(grepl("Silver",Delays$Incident),1,0)
+Delays$Orange<-ifelse(grepl("Orange",Delays$Incident),1,0)
+Delays$Blue<-ifelse(grepl("Blue",Delays$Incident),1,0)
+Delays$Red<-ifelse(grepl("Red",Delays$Incident),1,0)
+Delays$Yellow<-ifelse(grepl("Yellow",Delays$Incident),1,0)
+Delays$Green<-ifelse(grepl("Green",Delays$Incident),1,0)
 
-DelayV2$Bound<-ifelse(grepl("Largo Town Center-bound",DelayV2$Incident),"Largo",
-              ifelse(grepl("Largo-bound",DelayV2$Incident),"Largo",
-                ifelse(grepl("New Carrollton-bound",DelayV2$Incident),"New Carrollton",
-                ifelse(grepl("New Carrolton-bound",DelayV2$Incident),"New Carrollton",
-                  ifelse(grepl("Greenbelt-bound",DelayV2$Incident),"Greenbelt",
-                    ifelse(grepl("Glenmont-bound",DelayV2$Incident),"Glenmont",
-                      ifelse(grepl("Shady Grove-bound",DelayV2$Incident),"Shady Grove",
-                      ifelse(grepl("Shady Grove- bound",DelayV2$Incident),"Shady Grove",
-                        ifelse(grepl("Wiehle-Reston East-bound",DelayV2$Incident),"Wiehle-Reston East",
-                          ifelse(grepl("Vienna-bound",DelayV2$Incident),"Vienna",
-                            ifelse(grepl("Franconia-Springfield-bound",DelayV2$Incident),"Franconia-Springfield",
-                              ifelse(grepl("Branch Avenue-bound",DelayV2$Incident),"Branch Avenue",
-                              ifelse(grepl("Branch Ave-bound",DelayV2$Incident),"Branch Avenue",
-                                ifelse(grepl("Huntington-bound",DelayV2$Incident),"Huntington",
-                                  ifelse(grepl("Grosvenor-Strathmore-bound",DelayV2$Incident),"Grosvenor-Strathmore",
-                                  ifelse(grepl("Grosvenor-bound",DelayV2$Incident),"Grosvenor-Strathmore",
-                                    ifelse(grepl("Mt. Vernon Square-bound",DelayV2$Incident),"Mt. Vernon Square",
-                                    ifelse(grepl("Mt. Vernon-bound",DelayV2$Incident),"Mt. Vernon Square",
-                                    ifelse(grepl("Mt. Vernon Sq-bound",DelayV2$Incident),"Mt. Vernon Square",
-                                      ifelse(grepl("Silver Spring-bound",DelayV2$Incident),"Silver Spring",
-                                        ifelse(grepl("Fort Totten-bound",DelayV2$Incident),"Fort Totten",
-                                        ifelse(grepl("Ft Totten-bound",DelayV2$Incident),"Fort Totten",
-                                        ifelse(grepl("Ft. Totten-bound",DelayV2$Incident),"Fort Totten",
-                                          ifelse(grepl("NoMa-Gallaudet-bound",DelayV2$Incident),"NoMa-Gallaudet",
-                                            ifelse(grepl("Medical Center-bound",DelayV2$Incident),"Medical Center",
-                                              ifelse(grepl("Friendship Heights-bound",DelayV2$Incident),"Friendship Height",
-                                                ifelse(grepl("Van Dorn Street-bound",DelayV2$Incident),"Van Dorn Street",
-                                                  ifelse(grepl("Judiciary Square-bound",DelayV2$Incident),"Judiciary Square",
-                                                    ifelse(grepl("Grosvenor -bound",DelayV2$Incident),"Grosvenor-Strathmore",
-                                                      ifelse(grepl("Branch Ave -bound",DelayV2$Incident),"Branch Avenue",
-                                                        ifelse(grepl("Greenbelt Ave-bound",DelayV2$Incident),"Greenbelt Avenue",
-                                                          ifelse(grepl("Van Ness-bound",DelayV2$Incident),"Van Ness",
+Delays$Bound<-ifelse(grepl("Largo Town Center-bound",Delays$Incident),"Largo",
+              ifelse(grepl("Largo-bound",Delays$Incident),"Largo",
+                ifelse(grepl("New Carrollton-bound",Delays$Incident),"New Carrollton",
+                ifelse(grepl("New Carrolton-bound",Delays$Incident),"New Carrollton",
+                  ifelse(grepl("Greenbelt-bound",Delays$Incident),"Greenbelt",
+                    ifelse(grepl("Glenmont-bound",Delays$Incident),"Glenmont",
+                      ifelse(grepl("Shady Grove-bound",Delays$Incident),"Shady Grove",
+                      ifelse(grepl("Shady Grove- bound",Delays$Incident),"Shady Grove",
+                        ifelse(grepl("Wiehle-Reston East-bound",Delays$Incident),"Wiehle-Reston East",
+                          ifelse(grepl("Vienna-bound",Delays$Incident),"Vienna",
+                            ifelse(grepl("Franconia-Springfield-bound",Delays$Incident),"Franconia-Springfield",
+                              ifelse(grepl("Branch Avenue-bound",Delays$Incident),"Branch Avenue",
+                              ifelse(grepl("Branch Ave-bound",Delays$Incident),"Branch Avenue",
+                                ifelse(grepl("Huntington-bound",Delays$Incident),"Huntington",
+                                  ifelse(grepl("Grosvenor-Strathmore-bound",Delays$Incident),"Grosvenor-Strathmore",
+                                  ifelse(grepl("Grosvenor-bound",Delays$Incident),"Grosvenor-Strathmore",
+                                    ifelse(grepl("Mt. Vernon Square-bound",Delays$Incident),"Mt. Vernon Square",
+                                    ifelse(grepl("Mt. Vernon-bound",Delays$Incident),"Mt. Vernon Square",
+                                    ifelse(grepl("Mt. Vernon Sq-bound",Delays$Incident),"Mt. Vernon Square",
+                                      ifelse(grepl("Silver Spring-bound",Delays$Incident),"Silver Spring",
+                                        ifelse(grepl("Fort Totten-bound",Delays$Incident),"Fort Totten",
+                                        ifelse(grepl("Ft Totten-bound",Delays$Incident),"Fort Totten",
+                                        ifelse(grepl("Ft. Totten-bound",Delays$Incident),"Fort Totten",
+                                          ifelse(grepl("NoMa-Gallaudet-bound",Delays$Incident),"NoMa-Gallaudet",
+                                            ifelse(grepl("Medical Center-bound",Delays$Incident),"Medical Center",
+                                              ifelse(grepl("Friendship Heights-bound",Delays$Incident),"Friendship Height",
+                                                ifelse(grepl("Van Dorn Street-bound",Delays$Incident),"Van Dorn Street",
+                                                  ifelse(grepl("Judiciary Square-bound",Delays$Incident),"Judiciary Square",
+                                                    ifelse(grepl("Grosvenor -bound",Delays$Incident),"Grosvenor-Strathmore",
+                                                      ifelse(grepl("Branch Ave -bound",Delays$Incident),"Branch Avenue",
+                                                        ifelse(grepl("Greenbelt Ave-bound",Delays$Incident),"Greenbelt Avenue",
+                                                          ifelse(grepl("Van Ness-bound",Delays$Incident),"Van Ness",
                                         "N/A"))))))))))))))))))))))))))))))))
 
 
 ### Delay Stats ###
-aggregate(DelayV2$Delay, by=list(DelayV2$Bound), FUN=mean, na.rm=TRUE)
-aggregate(DelayV2$Delay, by=list(DelayV2$TimeGroup), FUN=mean, na.rm=TRUE)
-count(DelayV2$Delay, c('DelayV2$TimeGroup'))
-aggregate(DelayV2$Delay, by=list(DelayV2$DayType), FUN=mean, na.rm=TRUE)
-count(DelayV2$Delay, c('DelayV2$DayType'))
-aggregate(DelayV2$Delay, by=list(DelayV2$Day), FUN=mean, na.rm=TRUE)
-count(DelayV2$Delay, c('DelayV2$Day'))
-aggregate(DelayV2$Delay, by=list(DelayV2$Yr,DelayV2$Month), FUN=mean, na.rm=TRUE)
-count(DelayV2$Delay, c('DelayV2$Yr','DelayV2$Month'))
+aggregate(Delays$Delay, by=list(Delays$Yr), FUN=sum, na.rm=TRUE)
+count(Delays$Delay, c('Delays$Yr'))
+aggregate(Delays$Delay, by=list(Delays$Yr,Delays$Month), FUN=sum, na.rm=TRUE)
+count(Delays$Delay, c('Delays$Month','Delays$Yr'))
+aggregate(Delays$Delay, by=list(Delays$TimeGroup), FUN=mean, na.rm=TRUE)
+count(Delays$Delay, c('Delays$TimeGroup'))
+aggregate(Delays$Delay, by=list(Delays$DayType), FUN=mean, na.rm=TRUE)
+count(Delays$Delay, c('Delays$DayType'))
+aggregate(Delays$Delay, by=list(Delays$Day), FUN=mean, na.rm=TRUE)
+count(Delays$Delay, c('Delays$Day'))
 
-Saturday<-subset(DelayV2,DelayV2$Day=="Saturday")
+DelayTime<-subset(Delays,!is.na(Delays$Delay))
+count(DelayTime$Delay, c('DelayTime$Yr'))
+table(DelayTime$Issue,DelayTime$Yr)
 
-DelaySums<-DelayV2[c("Silver","Red","Orange","Blue","Yellow","Green","Delay")]
+Schedule15<-subset(DelayTime,DelayTime$Issue=="Schedule" & DelayTime$Yr=="2015")
+DelayTime$DNO<-ifelse(grepl("did not operate",DelayTime$Incident),1,0)
+table(DelayTime$DNO,DelayTime$Yr)
+
+#Delays by Line
+DelaySums<-Delays[c("Silver","Red","Orange","Blue","Yellow","Green","Delay")]
 colSums(DelaySums, na.rm=TRUE)
 Silver<-subset(DelaySums,DelaySums$Silver==1 & !is.na(DelaySums$Delay))
 mean(Silver$Delay)
